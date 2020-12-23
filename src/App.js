3558541e9b12267a2caf88a4cfa23edbb1379346
Alexander Koik-Cestone <akoikcestone@gmail.com>
2020-12-23 03:23:50 -0500

    error boundary example

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -54,12 +55,14 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             return (
-              <Person 
+              <ErrorBoundary key={person.id}>
+                <Person 
                 onClick={() => {this.deletePersonHandler(index)}}
                 name={person.name} 
                 age={person.age}
-                key={person.id}
-                changed={(event) => {this.nameChangedHandler(event, person.id)}}></Person>
+                changed={(event) => {this.nameChangedHandler(event, person.id)}}>
+                </Person>
+              </ErrorBoundary>
             );
           })}
         </div>        
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,6 +3,13 @@ import React from 'react';
 import classes from './Person.css';
 
 const person = (props) => {
+    const rnd = Math.random();
+
+    // simulated error 
+    if (rnd > 0.7) {
+        throw new Error('Something went wrong.');
+    }
+
     return (
         <div className={classes.Person}>
             <p onClick={props.onClick}>I'm {props.name} and I am {props.age} years old!</p>
