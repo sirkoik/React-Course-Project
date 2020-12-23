3ffba477713737aa970b0337f4dc7df0f7626521
Alexander Koik-Cestone <akoikcestone@gmail.com>
2020-12-23 04:28:04 -0500

    Added getDerivedStateFromError

--- src/ErrorBoundary/ErrorBoundary.js
+++ src/ErrorBoundary/ErrorBoundary.js
@@ -5,6 +5,10 @@ class ErrorBoundary extends Component {
         errorMessage: ''
     }
 
+    static getDerivedStateFromError(error) {
+        return { hasError:true }
+    }
+
     componentDidCatch = (error, info) => {
         this.setState({
             hasError: true,
@@ -15,7 +19,10 @@ class ErrorBoundary extends Component {
     render() {
         if (this.state.hasError) {
             return (
-                <h1>{this.state.errorMessage.message}</h1>
+                <div>
+                    <h1>{this.state.errorMessage.message}</h1>
+                    <p>This error rendered correctly.</p>
+                </div>
             );
         } else {
             return this.props.children;
