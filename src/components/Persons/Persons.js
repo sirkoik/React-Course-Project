7a6c5e7acb16875fa820440393bc404f6b80e4d6
Alexander Koik-Cestone <akoikcestone@gmail.com>
2021-01-23 15:50:18 -0500

    Class-based lifecycle hooks

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => {
-    console.log('[Persons.js] rendering...');
+class Persons extends Component {
+    // should have an initial state in order to use this.
+    // this is the first lifecycle hook that runs.
+    // static getDerivedStateFromProps(props, state) {
+    //     console.log('[Persons.js] getDerivedStateFromProps')
 
-    return props.persons.map((person, index) => {
-        return (
-            <Person 
-            onClick={() => {props.clicked(index)}}
-            name={person.name} 
-            age={person.age}
-            key={person.id}
-            changed={(event) => {props.changed(event, person.id)}}></Person>
-        );
-    });
-};
+    //     return state;
+    // }
 
-export default persons;
+    // triggers when component may be receving new props.
+    // deprecated
+    componentWillReceiveProps(props) {
+        console.log('[Persons.js] componentWillReceiveProps', props);
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[Persons.js] shouldComponentUpdate');
+
+        return true;
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('[Persons.js] getSnapshotBeforeUpdate');
+        return { message: 'snapshot!'};
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('[Persons.js] componentDidUpdate');
+        console.log(snapshot);
+    }
+
+    render() {
+        console.log('[Persons.js] rendering...');
+
+        return this.props.persons.map((person, index) => {
+            return (
+                <Person 
+                onClick={() => {this.props.clicked(index)}}
+                name={person.name} 
+                age={person.age}
+                key={person.id}
+                changed={(event) => {this.props.changed(event, person.id)}}></Person>
+            );
+        });
+    }
+}
+
+export default Persons;
--- src/containers/App.js
+++ src/containers/App.js
@@ -23,8 +23,19 @@ class App extends Component {
     console.log('[App.js] getDerivedStateFromProps', props);
   }
 
+  // only older versions of React support this
   componentDidMount() {
     console.log('[App.js] componentDidMount');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate');
+
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate');
   }
 
   togglePersonsHandler = () => {
